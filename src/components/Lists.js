1c8e9b19853d15b4b4283165b65030e0c405cdaf
vanshika0606 <91212347+vanshika0606@users.noreply.github.com>
2022-02-19 14:28:34 +0530

    Update Lists.js

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,33 +1,72 @@
 import React from 'react'
 
 export default function Lists() {
+  let parsedData=[
+    {
+      "id": 1,
+      "email": "george.bluth@reqres.in",
+      "first_name": "George",
+      "last_name": "Bluth",
+      "avatar": "https://reqres.in/img/faces/1-image.jpg"
+    },
+    {
+      "id": 2,
+      "email": "janet.weaver@reqres.in",
+      "first_name": "Janet",
+      "last_name": "Weaver",
+      "avatar": "https://reqres.in/img/faces/2-image.jpg"
+    },
+    {
+      "id": 3,
+      "email": "emma.wong@reqres.in",
+      "first_name": "Emma",
+      "last_name": "Wong",
+      "avatar": "https://reqres.in/img/faces/3-image.jpg"
+    },
+    {
+      "id": 4,
+      "email": "eve.holt@reqres.in",
+      "first_name": "Eve",
+      "last_name": "Holt",
+      "avatar": "https://reqres.in/img/faces/4-image.jpg"
+    },
+    {
+      "id": 5,
+      "email": "charles.morris@reqres.in",
+      "first_name": "Charles",
+      "last_name": "Morris",
+      "avatar": "https://reqres.in/img/faces/5-image.jpg"
+    },
+    {
+      "id": 6,
+      "email": "tracey.ramos@reqres.in",
+      "first_name": "Tracey",
+      "last_name": "Ramos",
+      "avatar": "https://reqres.in/img/faces/6-image.jpg"
+    }
+  ];
   return (
     <>
     <div id="table">
-      <div className='boxx' >
-        <div id="image">
-          <img src="https://reqres.in/img/faces/1-image.jpg" ></img>
-        </div>
-         
-          <p>
-           george smith
-          </p>
-          <p>
-          george.bluth@reqres.in
-          </p>
-       
-      </div>
-      <div className='boxx' >
-        <div id="image">
-          <img src="https://reqres.in/img/faces/1-image.jpg" ></img>
-        </div>
+     {parsedData.map((user,id)=>{
+       return <div className='boxx' key={id}>
+       <div id="image">
+         <img src={user.avatar} ></img>
+       </div>
         
-      </div>
-      <div className='boxx' >
-        <div id="image">
-          <img src="https://reqres.in/img/faces/1-image.jpg" ></img>
-        </div>
-      </div>
+         <p>
+          {user.first_name} {user.last_name}
+         </p>
+         <p>
+         {user.email}
+         </p>
+      
+     </div>
+     })
+
+     }
+      
+      
       
     </div>
     </>
